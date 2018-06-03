c6987d2b7b9a1fae13748c16e464c0b476d70442
Unknown <torrewz@gmail.com>
2018-06-03 18:41:16 -0500

    Parte del sharoleo

--- ProyectoFinal/sistema.php
+++ ProyectoFinal/sistema.php
@@ -1,70 +1,148 @@
 <?php
-
-    include('login.php');
-
+//include('login.php');
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta lang="es"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Sistema de Facturacion</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="estilos.css" />
-    <script src="funciones.js"></script>
-</head>
-<body>
-    <h1>Sistema de Facturacion</h1>
-    <h2>Datos del emisor</h2>
-
-    <form action="functions.php" method="post">
+    <head>
+        <meta charset="utf-8" />
+        <meta lang="es"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Sistema de Facturacion</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="estilos.css" />
+        <script src="funciones.js"></script>
+    </head>
+    <body>
+        <h1>Sistema de Facturacion</h1>
         
-        <p>RFC: </p><p><input type="text" id="rfcEmisor" value="<?php echo($_SESSION['rfc']); ?>" disabled></p>
-        <p>Nombre o Razon Social:</p><p><input id="nombreEmisor" type="text" value="<?php echo($_SESSION['nombreEmisor']); ?>"></p>
-
-        <select id="regimenFiscal">
-            <option value=""selected>Seleccione...</option>
-        </select>
-        <select id="tipoComprobante">
-            <option value="" selected>Seleccione...</option>
-            <option value="E">Egreso</option>
-            <option value="I">Ingreso</option>
-            <option value="N">Nomina</option>
-            <option value="P">Pago</option>
-            <option value="T">Traslado</option>
-        </select>
-
-    </form>
-
-
-    <h2>Datos del Receptor</h2>
-
-
-    <form>
+        <form action="functions.php" method="post" style="height: 230px;">
+            <h2>Datos del emisor</h2>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    RFC:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="rfcEmisor" value="<?php echo($_SESSION['rfc']); ?>" disabled>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Nombre o Razon Social:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="nombreEmisor" value="<?php echo($_SESSION['nombreEmisor']); ?>">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Regimen Fiscal:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="regimenFiscal">
+                        <option value="" selected>Seleccione...</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Tipo de Comprobante:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="tipoComprobante">
+                        <option value="">Seleccione...</option>
+                        <option value="E">Egreso</option>
+                        <option value="I">Ingreso</option>
+                        <option value="N">Nomina</option>
+                        <option value="P">Pago</option>
+                        <option value="T">Traslado</option>
+                    </select>
+                </div>
+            </div>
+        </form>
         
-         <p>RFC registrado: </p><p><input type="text" id="rfcReceptor" placeholder="RFC receptor" onkeyup="receptor()"></p>
-        <p>Nombre o Razon Social</p><p><input type="text" id="nombreReceptor" value="Nombre del Receptor" disabled></p>
-        <select id="residenciaFiscal">
-            <option value="residencia">Residencia Fiscal</option>
-        </select>
-        <select id="noRegistro">
-            <option value="registro">No. de registro de identidad fiscal</option>
-        </select>
-        <select id="cfdi">
-            <option value="cfdi1">Uso de CFDI</option>
-            <option value=D01>Honorarios medicos, dentales y gastos hospitalarios.</option><option value=D02>Gastos medicos por incapacidad o discapacidad</option><option value=D03>Gastos funerales.</option><option value=D04>Donativos.</option><option value=D05>Intereses reales efectivamente pagados por cr?ditos hipotecarios (casa habitaci?n).</option><option value=D06>Aportaciones voluntarias al SAR.</option><option value=D07>Primas por seguros de gastos m?dicos.</option><option value=D08>Gastos de transportaci?n escolar obligatoria.</option><option value=D09>Dep?sitos en cuentas para el ahorro, primas que tengan como base planes de pensiones.</option><option value=D10>Pagos por servicios educativos (colegiaturas)</option><option value=G01>Adquisici?n de mercancias</option><option value=G02>Devoluciones, descuentos o bonificaciones</option><option value=G03>Gastos en general</option><option value=I01>Construcciones</option><option value=I02>Mobilario y equipo de oficina por inversiones</option><option value=I03>Equipo de transporte</option><option value=I04>Equipo de computo y accesorios</option><option value=I05>Dados, troqueles, moldes, matrices y herramental</option><option value=I06>Comunicaciones telef?nicas</option><option value=I07>Comunicaciones satelitales</option><option value=I08>Otra maquinaria y equipo</option><option value=P01>Por definir</option>
-        </select>
-        <br><br>
-         <input type="button" name="siguiente" value="siguiente" onclick="emisorReceptor()"; >
-
-    </form>
-    <h2><a href = "logout.php">Salir</a></h2>
-
-
-    <script type="text/javascript">
-    window.addEventListener('load', rellenarReg, true);
-    </script>
-</body>
+        <form style="height: 300px;">
+            <h2>Datos del Receptor</h2>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    RFC registrado:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="rfcReceptor" placeholder="RFC receptor" onkeyup="receptor()">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Nombre o Razon Social:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="nombreReceptor" placeholder="Nombre del Receptor">
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Residencia Fiscal:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="residenciaFiscal">
+                        <option value="residencia">Residencia Fiscal</option>
+                    </select>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Numero de Registro:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="noRegistro">
+                        <option value="registro">No. de registro de identidad fiscal</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Numero de Registro:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="cfdi">
+                        <option value="cfdi1">Uso de CFDI</option>
+                        <option value=D01>Honorarios medicos, dentales y gastos hospitalarios.</option>
+                        <option value=D02>Gastos medicos por incapacidad o discapacidad</option>
+                        <option value=D03>Gastos funerales.</option>
+                        <option value=D04>Donativos.</option>
+                        <option value=D05>Intereses reales efectivamente pagados por cr?ditos hipotecarios (casa habitaci?n).</option>
+                        <option value=D06>Aportaciones voluntarias al SAR.</option>
+                        <option value=D07>Primas por seguros de gastos m?dicos.</option>
+                        <option value=D08>Gastos de transportaci?n escolar obligatoria.</option>
+                        <option value=D09>Depósitos en cuentas para el ahorro, primas que tengan como base planes de pensiones.</option><option value=D10>Pagos por servicios educativos (colegiaturas)</option>
+                        <option value=G01>Adquisición de mercancias</option>
+                        <option value=G02>Devoluciones, descuentos o bonificaciones</option>
+                        <option value=G03>Gastos en general</option>
+                        <option value=I01>Construcciones</option>
+                        <option value=I02>Mobilario y equipo de oficina por inversiones</option>
+                        <option value=I03>Equipo de transporte</option>
+                        <option value=I04>Equipo de computo y accesorios</option>
+                        <option value=I05>Dados, troqueles, moldes, matrices y herramental</option>
+                        <option value=I06>Comunicaciones telef?nicas</option>
+                        <option value=I07>Comunicaciones satelitales</option>
+                        <option value=I08>Otra maquinaria y equipo</option>
+                        <option value=P01>Por definir</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    .
+                </label>
+                <div class="col-sm-8">
+                    <input type="button" name="siguiente" value="siguiente" onclick="emisorReceptor();" >
+                </div>
+            </div>
+            
+        </form>
+        <h2><a href = "logout.php">Salir</a></h2>
+        <script type="text/javascript">
+        window.addEventListener('load', rellenarReg, true);
+        </script>
+    </body>
 </html>
--- ProyectoFinal/sistema2.php
+++ ProyectoFinal/sistema2.php
@@ -1,76 +1,151 @@
 <?php
-
-    //include('login.php');
-
+//include('login.php');
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta lang="es"/>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Sistema de Facturacion</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="estilos.css" />
-    <script src="funciones.js"></script>
-</head>
-<body>
-    <h1>Sistema de Facturacion</h1>
-    <h2>Comprobante</h2>
-
-    <form action="functions.php" method="post">
+    <head>
+        <meta charset="utf-8" />
+        <meta lang="es"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Sistema de Facturacion</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="estilos.css" />
+        <script src="funciones.js"></script>
+    </head>
+    <body>
+        <h1>Sistema de Facturacion</h1>
         
-        <p>Fecha y hora de Expedición: </p><p><input type="datetime-local" id="fechaExpedicion" required></p>
-
-        <p>Lugar de Expedición: </p><p><input type="text" id="lugarExpedicion" required></p>
-
-        <p>Serie: </p><p><input type="text" id="serie" required></p>
-
-        <p>Folio: </p><p><input type="text" id="folio" required></p>
-        
-        <p>Moneda: </p>
-        <select id="moneda" required>
-            <option value="moneda">Peso Mexicano MXN</option>
-        </select>
-
-        <p>Tipo de Cambio: </p><p><input type="text" id="tipoCambio" required></p>
-        
-        <p>Forma de PAgo: </p>
-        <select id="formaPago" required>
-            <option value="formaPago">01 Efectivo</option>
-        </select>
-
-        <p>Metodo de Pago: </p>
-        <select id="metodoPago" required>
-            <option value="metodoPago">01 Pago en una sola exhibición</option>
-        </select>
-
-        <p>Confirmación: </p><p><input type="text" id="confirmacion" required></p>
-
-        <p>Condiciones de Pago: </p><p><input type="text" id="condiciones" required></p>
-    </form>
-
-
-    <h2>CFDI Relacionados </h2>
-
-
-    <form>
-        
-        <p>Tipo de Relación: </p>
-        <select id="metodoPago" required>
-            <option value="metodoPago">01 Pago en una sola exhibición</option>
-        </select>
-
-        <p>Conceptos: </p><p><input type="button" id="conceptos" onclick="" value="Agregar"></p>
-        <div class="conceptos">
+        <form action="functions.php" method="post" style="height: 450px;">
+            <h2>Comprobante</h2>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Fecha y hora de Expedición:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="datetime-local" id="fechaExpedicion" required>
+                </div>
+                
+            </div>
             
-        </div>
-
-
-         
-        <input type="button" onclick="" value="Continuar">
-    </form>
-    <h2><a href = "logout.php">Salir</a></h2>
-</body>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Lugar de Expedición:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="lugarExpedicion" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Serie:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="serie" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Folio:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="folio" required>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Moneda:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="moneda" required>
+                        <option value="moneda">Peso Mexicano MXN</option>
+                    </select>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Tipo de Cambio:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="tipoCambio" required>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Forma de Pago:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="formaPago" required>
+                        <option value="formaPago">01 Efectivo</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Metodo de Pago:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="metodoPago" required>
+                        <option value="metodoPago">01 Pago en una sola exhibición</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Confirmación:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="confirmacion" required>
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Condiciones de Pago:
+                </label>
+                <div class="col-sm-8">
+                    <input class="form-control" type="text" id="condiciones" required>
+                </div>
+            </div>
+        </form>
+        
+        <form>
+            <h2>CFDI Relacionados </h2>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Tipo de Relación:
+                </label>
+                <div class="col-sm-8">
+                    <select class="form-control" id="metodoPago" required>
+                        <option value="metodoPago">01 Pago en una sola exhibición</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    Conceptos:
+                </label>
+                <div class="col-sm-8">
+                    <input type="button" id="conceptos" onclick="" value="Agregar">
+                </div>
+                <div class="conceptos">
+                    
+                </div>
+            </div>
+            
+            <div class="form-group">
+                <label class="col-sm-4 control-label">
+                    .
+                </label>
+                <div class="col-sm-8">
+                    <input type="button" onclick="" value="Continuar">
+                </div>
+                
+            </div>
+        </form>
+        <br>
+        <br>
+        <h2><a href = "logout.php">Salir</a></h2>
+    </body>
 </html>
