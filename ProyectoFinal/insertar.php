d230304afe354171800b62e2ac72be29d0aed578
Unknown <torrewz@gmail.com>
2018-06-05 21:35:36 -0500

    Inserts culeros x2

--- ProyectoFinal/insertar.php
+++ ProyectoFinal/insertar.php
@@ -4,25 +4,54 @@
 
 	$variables = parse_ini_file('Config.ini');
 
+	session_start();
+
+	$rfcEmisor = $_POST['rfcEmisor'];
+	$nombreEmisor = $_POST['nombreEmisor'];
+	$regimenFiscal = $_POST['regimenFiscal'];
+	$tipoComprobante= $_POST['tipoComprobante'];
+
+	$rfCReceptor = $_POST['rfCReceptor'];
+	$nombreReceptor = $_POST['nombreReceptor'];
+	$residenciaFiscal = $_POST['residenciaFiscal'];
+	$noRegistro = $_POST['noRegistro'];
+	$cfdi = $_POST['cfdi'];
+
+	$fechaExpedicion = $_POST['fechaExpedicion'];
+	$lugarExpedicion = $_POST['lugarExpedicion'];
+	$serie = $_POST['serie'];
+	$folio = $_POST['folio'];
+	$moneda = $_POST['moneda'];
+	$tipoCambio = $_POST['tipoCambio'];
+	$formaPago = $_POST['formaPago'];
+	$metodoPago = $_POST['metodoPago'];
+	$confirmacion = $_POST['confirmacion'];
+	$condiciones = $_POST['condiciones'];
+	$tipoRelacion = $_POST['tipoRelacion'];
+	$subtotal = $_POST['subtotal'];
+	$impuestosTras = $_POST['impuestosTras'];
+	$total = $_POST['total'];
+
+	$conceptos = json_decode($_POST['conceptos'], true);
 
 
-	echo "aqui vergas";
     $pdo = conectarMysqlAnx($variables);
     //Comprobante
-    $query = "INSERT INTO (Version,	Serie,	Folio,	Fecha,	Sello,	FormaPago,	Certificado,	NoCertificado,	CondicionesDePago,	SubTotal,	Moneda,	TipoCambio,	Total,	TipoDeComprobante,	MetodoPago,	LugarExpedicion,	Confirmacion,	RFCEmisor,	RFCReceptor) VALUES (3.3,'".$_POST['serie']."','".$_POST['folio']."','".$_POST['fechaExpedicion']."','".'ASHCXJ3243ASDAS23-XE'."','".$_POST['formaPago']."',ASHCXJ3243ASDAS23','45,'".$_POST['condiciones']."','".$_POST['subtotal']."','".$_POST['moneda']."','".$_POST['tipoCambio']."','".$_POST['total']."','".$_POST['tipoComprobante']."','".$_POST['metodoPago']."','".$_POST['lugarExpedicion']."','".$_POST['confirmacion']."','".$_POST['rfcEmisor']."','".$_POST['rfCReceptor']."')'";
+    $query = "INSERT INTO (Version,	Serie,	Folio,	Fecha,	Sello,	FormaPago,	Certificado, NoCertificado,	CondicionesDePago,	SubTotal,	Moneda,	TipoCambio,	Total,	TipoDeComprobante,	MetodoPago,	LugarExpedicion,	Confirmacion,	RFCEmisor,	RFCReceptor) VALUES ('3.3', $serie, $folio, $fechaExpedicion, 'ASHCXJ3243ASDAS23-XE', $formaPago, 'ASHCXJ3243ASDAS23','45', $condiciones, $subtotal, $moneda, $tipoCambio, $total, $tipoComprobante,$metodoPago, $lugarExpedicion, $confirmacion, $rfcEmisor, $rfCReceptor);";
 
-    echo($query);
+    print_r($query);
+    $query = $pdo->prepare($query);
+    $query->execute();
 
-/*
+
     //Conceptos
-    $query = "SELECT idComprobante FROM comprobante WHERE Folio = $_POST['folio']";
+    $query = "SELECT idComprobante FROM comprobante WHERE Folio = $folio";
     $query = $pdo->prepare($query);
     $query->execute();
     $idComprobante = $query->fetchAll();
    
-    $conceptos = json_decode($_POST['conceptos'], true);
     $query = "INSERT INTO (ClaveProdServ, NoIdentificacion, Cantidad, Unidad, Descripcion, ValorUnitario, Importe, idComprobante) concepto VALUES ($conceptos=>claveprod, $conceptos=>noIdentificacion,$conceptos=>cantidad,$conceptos=>unidad,$conceptos=>descripcion,$conceptos=>valorU,$conceptos=>importe,$idComprobante);";
 
     $query = $pdo->prepare($query);
-    $query->execute();*/
+    $query->execute();
  ?>
