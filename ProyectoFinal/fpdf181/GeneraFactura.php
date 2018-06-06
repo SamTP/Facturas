ce200b2214b34f6b9424551819af2951ab76821e
SamTP <39800905+SamTP@users.noreply.github.com>
2018-06-06 10:24:36 -0500

    Cambios Factura

--- ProyectoFinal/fpdf181/GeneraFactura.php
+++ ProyectoFinal/fpdf181/GeneraFactura.php
@@ -2,33 +2,24 @@
 
 require('fpdf.php');
 date_default_timezone_set('America/Mexico_City');
-session_start();
 
-$variables = parse_ini_file('../Config.ini');
+
+function generaFactura($conceptos,$rfcEmisor,$nombreEmisor,$rfcReceptor,
+$nombreReceptor,$serie,$folio,$usoCDFI,$moneda,$regimen,
+$subtotal,$iva,$total,$claveprod,$descripcion,$cantidad,$valorU){
+
+$variables = parse_ini_file('Config.ini');
 $dir = $variables['DirectorioRaiz'];
 $sub = $variables['SubCarpeta1'];
-
+$folioFiscal="rel312iuvboerv-q12veqw";
 
 //Variables
-$rfcEmisor = "";
-$nombreEmisor = "12";
-$rfcReceptor = "12";
-$nombreReceptor = "12";
+
+ 
 $fecha = date("d/m/Y G:i");
-$serie = "12";
 $numSerie = "12";
-$folio = "12";
-$folioFiscal = "12";
-$usoCDFI = "12";
-$moneda = "12";
-$regimen = "12";
-$productos = "";
-$subtotal = "";
-$iva = "";
-$total = "";
+
 $fechaCert = date("d/m/Y G:i");
-
-
 
 $pdf = new FPDF();
 $pdf->AddPage();
@@ -99,13 +90,13 @@ $pdf->Cell(105, 10, utf8_decode($texto1), 0, 0, "L");
 
 $texto1 = "RFC EMISOR: " . $rfcEmisor;
 $pdf->SetXY(.3, 10.5);
-$pdf->SetFont('Times', 'B', 11);
+$pdf->SetFont('Times', 'B', 10);
 $pdf->MultiCell(65, 5, $texto1, 0, "L");
 
-$texto1 = "NOMBRE EMISOR: " . $nombreEmisor;
+$texto1 = "NOMBRE EMISOR:" . $nombreEmisor;
 $pdf->SetXY(.3, 15.5);
-$pdf->SetFont('Times', 'B', 11);
-$pdf->MultiCell(70, 5, $texto1, 0, "L");
+$pdf->SetFont('Times', 'B',11);
+$pdf->MultiCell(75, 5, $texto1, 0, "L");
 
 $texto1 = "Uso CFDI:" . $usoCDFI;
 $pdf->SetXY(.3, 60.5);
@@ -141,39 +132,39 @@ $pdf->Cell(70, 10, "Descripcion", 1, 0, "C", true);
 $pdf->Cell(30, 10, "Cant.", 1, 0, "C", true);
 $pdf->Cell(20, 10, utf8_decode("$/Ud"), 1, 0, "C", true);
 $pdf->Cell(30, 10, "Subt.", 1, 1, "C", true);
-$total = 5.2;
+
 
 // Los datos (en negro)
 $pdf->SetTextColor(0, 0, 0);
 
 
 $pdf->SetX(5);
-$pdf->Cell(50, 10, "123548", 1, 0, "L");
-$pdf->Cell(70, 10, "Bla bla", 1, 0, "C");
-$pdf->Cell(30, 10, number_format(1), 1, 0, "C");
-$pdf->Cell(20, 10, number_format(5.2, 1), 1, 0, "R");
-$pdf->Cell(30, 10, number_format(5.2, 2), 1, 1, "R");
+$pdf->Cell(50, 10, $claveprod,  1, "L");
+$pdf->Cell(70, 10, $descripcion,  1, "C");
+$pdf->Cell(30, 10, $cantidad,  1, "C");
+$pdf->Cell(20, 10, $valorU,  1, 0, "R");
+$pdf->Cell(30, 10, $subtotal,1, 1, "R");
 
 
 // Los totales, IVAs y demás
 $pdf->SetX(125);
 $pdf->Cell(50, 10, "Subtotal:", 1, 0, "C");
-$pdf->Cell(30, 10, number_format($subtotal, 2), 1, 1, "R");
+$pdf->Cell(30, 10, $subtotal, 2, 1, 1, "R");
 $pdf->SetX(125);
 $pdf->Cell(50, 10, "IVA (16%): ", 1, 0, "C");
-$pdf->Cell(30, 10, number_format($iva, 2), 1, 1, "R");
+$pdf->Cell(30, 10, $iva, 2, 1, 1, "R");
 $pdf->SetX(125);
 $pdf->Cell(50, 10, "Total:", 1, 0, "C");
-$pdf->Cell(30, 10, number_format($total, 2), 1, 1, "R");
+$pdf->Cell(30, 10, $total, 2, 1, 1, "R");
 
 //Cantidad Con letra
 $pdf->SetXY(5, 95);
-$pdf->Cell(120, 20, "Total con letra: \n" . "Seisientos pesos 03/100 M.N.", 1, 0, 'L');
+$pdf->Cell(120, 20, utf8_decode("Método de Pago: \n") . "Efectivo", 1, 0, 'L');
 
 //Metodo de Pago
 $pdf->SetXY(5, 115);
 $pdf->SetFont('Times', '', 10);
-$pdf->Cell(120, 10, utf8_decode("PAGO EN UNA SOLA EXHIBICIÓN ") . " PAGO EN EFECTIVO", 1, 0, 'L');
+$pdf->Cell(120, 10, utf8_decode("PAGO EN UNA SOLA EXHIBICIÓN ") , 1, 0, 'L');
 
 
 //Pie de Pagina
@@ -195,8 +186,9 @@ $pdf->Cell(175, 10, utf8_decode("tOSe+Ex/wvn33YIGwtfmrJwQ31Crd7II9VcHTOFhSdpzb")
 
 
 
-// Imprimimos el logo a 300 ppp
-$pdf->Image('CodigoQR.png', 5, 145, 70);
+// Imprimimos el QR 
+$imagen='C:/xampp/htdocs/Facturas/ProyectoFinal/fpdf181/img.png';
+$pdf->Image($imagen, 5, 145, 70);
 
 $pdf->SetXY(70, 155);
 $pdf->SetFont('Times', 'B', 11);
@@ -224,21 +216,21 @@ $pdf->Cell(70, 5, utf8_decode($fechaCert), 0, 0, 'L');
 
 
 // El documento enviado a la carpeta raiz
-$nombreArchivo = "Factura-" . $folio . ".pdf";
-$path = "../".$dir . "/" . $sub . "/" . $nombreArchivo;
-if (is_dir("../".$dir . "/" . $sub)) {
+$nombreArchivo = "Factura-" . $folio .$usoCDFI. ".pdf";
+$path = $dir . "/" . $sub . "/" . $nombreArchivo;
+if (is_dir($dir . "/" . $sub)) {
     $pdf->Output($path, 'F');
 
 } else {
-    if (is_dir("../".$dir)) {
-        mkdir("../".$dir . "/" . $sub);
+    if (is_dir($dir)) {
+        mkdir($dir . "/" . $sub);
         $pdf->Output($path, 'F');
     } else {
-        mkdir("../".$dir);
-        mkdir("../".$dir . "/" . $sub);
+        mkdir($dir);
+        mkdir($dir . "/" . $sub);
         $pdf->Output($path, 'F');
     }
 
 }
-
+}
 ?>
